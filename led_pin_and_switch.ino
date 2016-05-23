a021240a5cd7cd9de5b517770816ea1267d84e1d
Rchar <Ryan.Charles@mail.citytech.cuny.edu>
2016-05-23 15:37:04 -0400

    2 buttons 2 LED code

---
+++ led_pin_and_switch.ino
@@ -0,0 +1,42 @@
+int ledPin = 8;
+int ledPin2 = 9;
+int switchPin= 2;
+int switchPin2= 3;
+int buttonRead = 0;
+int buttonRead2 = 0;
+
+
+void setup() {
+  pinMode(switchPin,INPUT);
+  pinMode(ledPin,OUTPUT);
+  pinMode(switchPin2,INPUT);
+  pinMode(ledPin2,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //digitalWrite(ledPin,HIGH);
+
+    buttonRead = digitalRead(switchPin);
+  Serial.println(buttonRead);
+  
+  if(buttonRead == 1){
+    digitalWrite(ledPin,HIGH);
+  }
+  if (buttonRead == 0) {
+    digitalWrite(ledPin,LOW);
+    
+  }
+  
+  buttonRead2 = digitalRead(switchPin2);
+  Serial.println(buttonRead2);
+  
+  if(buttonRead == 1){
+    digitalWrite(ledPin2,HIGH);
+  }
+  if (buttonRead == 0) {
+    digitalWrite(ledPin2,LOW);
+    
+  }
+  
+}
