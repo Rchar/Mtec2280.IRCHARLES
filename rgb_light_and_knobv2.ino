938fda59826b9af519aba8409d42664ecf6fa0a8
Rchar <Ryan.Charles@mail.citytech.cuny.edu>
2016-05-23 15:47:38 -0400

    traffic light code

---
+++ rgb_light_and_knobv2.ino
@@ -0,0 +1,60 @@
+
+int sensorPin = 0;
+int lP1 = 2;
+int lP2 = 3; 
+int lP3 = 4; 
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+pinMode(lP1, OUTPUT);
+pinMode(lP2, OUTPUT);
+pinMode(lP3, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ int sensorPin = analogRead(A1);
+  Serial.println(sensorPin);
+ if ((sensorPin <= 254) &&(sensorPin > 10)) {
+  digitalWrite(lP1,HIGH);
+  digitalWrite(lP2,LOW);
+  digitalWrite(lP3,LOW);
+  
+}
+
+ else if ((sensorPin <= 511) &&(sensorPin > 255)) {
+  digitalWrite(lP1,HIGH);
+  digitalWrite(lP2,HIGH);
+  digitalWrite(lP3,LOW);
+ 
+}
+
+ else if ((sensorPin <=800 ) &&(sensorPin > 512)) {
+  digitalWrite(lP1,HIGH);
+  digitalWrite(lP2,HIGH);
+  digitalWrite(lP3,HIGH);
+  
+}
+
+ else if ((sensorPin <=1023 ) &&(sensorPin > 801)) {
+  digitalWrite(lP1,HIGH);
+  digitalWrite(lP2,HIGH);
+  digitalWrite(lP3,HIGH);
+ 
+
+}
+
+else {
+  digitalWrite(lP1,LOW);
+  digitalWrite(lP2,LOW);
+  digitalWrite(lP3,LOW);
+  
+
+  }
+}
+
